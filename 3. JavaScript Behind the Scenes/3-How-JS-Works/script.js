085262f433c4b35fc14e5d4cb58db3923ea57ac0
Whitley Blaine <whitleyblaine@gmail.com>
2017-10-25 01:46:53 -0400

    Finished Hoisting Examples

--- 3. JavaScript Behind the Scenes/3-How-JS-Works/script.js
+++ 3. JavaScript Behind the Scenes/3-How-JS-Works/script.js
@@ -1,13 +1,39 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+// FUNCTIONS
+calculateAge(1950);
+
+function calculateAge(year) {
+    console.log(2016-year);
+}
+
+
+// Does not work because retirement is a function expression, not a function declaration
+// retirement(1993);
+
+var retirement = function(year) {
+    console.log(65 - (2016 - year));
+}
 
 
 
 
 
+// VARIABLES
+
+// First console.log will log "undefined", which is different than if there was no var age in the code. because var age exists in the code, it is scanned in the creation phase (hoisting), and set to undefined
+console.log(age);
+var age = 23;
+console.log(age);
 
 
+function foo() {
+    var age = 65;
+    console.log(age);
+}
+foo();
+console.log(age);
 
 
 
