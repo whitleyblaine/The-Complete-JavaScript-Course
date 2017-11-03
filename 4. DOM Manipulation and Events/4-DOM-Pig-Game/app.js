114af3ad9a3c9b3ddb752a9894d8f3e1154736c4
Whitley Blaine <whitleyblaine@gmail.com>
2017-11-02 22:01:01 -0400

    Created global variables; created turn change, new game and dice roll functions

--- 4. DOM Manipulation and Events/4-DOM-Pig-Game/app.js
+++ 4. DOM Manipulation and Events/4-DOM-Pig-Game/app.js
@@ -8,3 +8,48 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+"use strict";
+
+var globalScores = [0, 0];
+var roundScores = [0, 0];
+var turn = 0;
+
+var newGameBtn = document.getElementsByClassName('btn-new')[0];
+var rollBtn = document.getElementsByClassName('btn-roll')[0];
+var holdBtn = document.getElementsByClassName('btn-hold')[0];
+var globalScoreDisplays = document.getElementsByClassName('player-score');
+var roundScoreDisplays = document.getElementsByClassName('player-current-score');
+var diceRollValue;
+
+var diceImage = document.getElementsByClassName('dice')[0];
+
+function turnChange() {
+  if (turn === 0) {
+    turn = 1;
+  } else {
+    turn = 0;
+  }
+}
+
+newGameBtn.onclick = function() {
+ globalScores = [0, 0];
+ roundScores = [0, 0];
+ globalScoreDisplays[0].innerHTML = '0';
+ globalScoreDisplays[1].innerHTML = '0';
+ roundScoreDisplays[0].innerHTML = '0';
+ roundScoreDisplays[1].innerHTML = '0';
+}
+
+rollBtn.onclick = function() {
+  diceRollValue = Math.floor(Math.random() * 7);
+
+  diceImage.src = "dice-" + diceRollValue + ".png";
+
+  if (diceRollValue != 1) {
+    roundScores[turn] += diceRollValue;
+  } else {
+    roundScores[turn] = 0;
+  }
+  roundScoreDisplays[turn].innerHTML = roundScores[turn];
+}
